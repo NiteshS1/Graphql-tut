629a3e7fd0e25f2c9febecf3b321f066af5f8eae
Nitesh <niti2002sh@gmail.com>
2025-04-03 15:42:49 +0530

    Added user auth

--- Second video/server/src/graphql/user/resolvers.ts
+++ Second video/server/src/graphql/user/resolvers.ts
@@ -1,9 +1,17 @@
-const queries = {};
+import UserService, { createUserPayload } from "../../services/user";
+
+const queries = {
+    getUserToken: async(_: any, payload: { email: string, password: string }) => {
+        const token = await UserService.getUserToken(payload);
+        return token;
+    }
+};
 
 const mutation = {
-    createUser: async(_: any, {}:{}) => {
-        return "randomid";
-    }
+    createUser: async(_: any, payload: createUserPayload) => {
+        const res = await UserService.createUser(payload);
+        return res.id;
+    },
 };
 
 export const resolvers = { queries, mutation };
